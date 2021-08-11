16253c99604b5e432e479ed651aece27fd443db0
Zulfathly <zulonaya@gmail.com>
2021-08-12 00:11:57 +0800

    Initialize project and app header

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import RMDBLogo from '../../images/react-movie-logo.svg';
+import TMDBLogo from '../../images/tmdb_logo.svg';
+
+// styles
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles"
+
+export const Header = () => {
+    return (
+        <Wrapper>
+            <Content>
+                <LogoImg src={RMDBLogo} alt="RMDB-Logo" />
+                <TMDBLogoImg src={TMDBLogo} alt="TMDB-Logo" />
+            </Content>
+        </Wrapper>
+    )
+}
