20d53bfab37d12d2121488522f53ed455c75fc95
zulfathly <zulonaya@gmail.com>
2021-08-14 22:37:24 +0800

    Added home js and rendered on app js

---
+++ src/components/Home.js
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react';
+//API
+import API from '../API'
+
+//config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+
+//Components
+
+//Hooks
+
+//Image
+import NoImage from '../images/no_image.jpg'
+
+export const Home = () => {
+    const [state, setState] = useState();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const fetchMovies = async (page, searchTerm = "") => {
+        try {
+            setError(false);
+            setLoading(true);
+
+            const movies = await API.fetchMovies(searchTerm, page);
+            console.log(movies);
+
+            setState(prev => ({
+                ...movies,
+                results:
+                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+            }));
+        }
+        catch (error) {
+            setError(true);
+        }
+        setLoading(false);
+    
+    }
+
+    //initial render
+    useEffect(() => {
+        fetchMovies(1);
+        
+    }, [])
+
+    console.log(state);
+
+    return <div>Homepage</div>
+}
