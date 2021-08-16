bbe6e903f7553444f88151dced62f99637348d12
zulfathly <zulonaya@gmail.com>
2021-08-16 21:41:13 +0800

    Added custom hooks to fetch one movie data

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+
+//config
+import { POSTER_SIZE, IMAGE_BASE_URL } from '../config';
+
+//components
+import { Grid } from './Grid';
+import { Spinner } from './Spinner';
+
+
+//Hooks
+import { useMovieFetch } from '../hooks/useMovieFetch'
+
+//Image
+import NoImage from '../images/no_image.jpg'
 
 export const Movie = () => {
+    const { movieId } = useParams();
+
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+
+    console.log(movie);
+    
     return (
+        <>
         <div>
             Moive
-        </div>
+            </div>
+        </>
     )
 }
